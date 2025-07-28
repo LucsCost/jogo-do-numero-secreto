0f77eb3c998046e3b90d2b5d4c560f3990f8ddf6
Lucas Costa <lucas2001costa2001@gmail.com>
2025-07-28 20:49:11 -0300

    aumentando limite para 100

--- Jogo número secreto (Alura) 2/app.js
+++ Jogo número secreto (Alura) 2/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
@@ -34,7 +34,7 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto e menor');
+            exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
         exibirTextoNaTela('p', 'O número secreto é maior.');
         }
